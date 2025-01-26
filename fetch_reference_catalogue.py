04a345502e4a297749a2d10a943d547205ddda4d
Morgan Mitchell <morgan_mitchell@outlook.com>
2025-01-26 17:34:52 +0000

    Use saved catalogue plus get information from images

--- fetch_reference_catalogue.py
+++ fetch_reference_catalogue.py
@@ -2,9 +2,14 @@ import argparse as ap
 from astropy.time import Time
 from astropy.table import Table, vstack
 from astropy.coordinates import SkyCoord
+import fitsio
 import astropy.units as u
 import numpy as np
 import warnings
+import pickle
+
+from jplephem.names import target_names
+from path import Path
 
 def generate_query_polygon(center, width, height, max_step_distance):
     """
@@ -55,22 +60,29 @@ def generate_query_polygon(center, width, height, max_step_distance):
     return SkyCoord(top + right + bottom + left)
 
 
-def query_tap(center, width, height, calibration_limits, blend_delta, step_distance=1 * u.degree):
-    polygon_points = generate_query_polygon(center, width, height, step_distance)
-    polygon_points = ', '.join(['{0}, {1}'.format(x.ra.to_value(u.degree), x.dec.to_value(u.degree)) for x in polygon_points])
-    query = f"""
-    SELECT source_id, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag, phot_bp_n_obs, phot_rp_n_obs, phot_variable_flag \
-        FROM gaiadr3.gaia_source \
-        WHERE CONTAINS(
-            POINT('ICRS', gaiadr3.gaia_source.ra, gaiadr3.gaia_source.dec), \
-            POLYGON('ICRS',{polygon_points}))=1 \
-        AND phot_g_mean_mag < {calibration_limits[1] + blend_delta} and phot_g_mean_mag > {calibration_limits[0] - blend_delta};
-    """
+def query_tap(out_path, center, width, height, calibration_limits, blend_delta, step_distance=1 * u.degree):
 
-    from astroquery.gaia import Gaia
-    job = Gaia.launch_job_async(query)
-    results = job.get_results()
-    Gaia.remove_jobs([job.jobid])
+    # store in the same directory as the out_path but with modified name
+    gaia_result_path = out_path.dirname() / f"{out_path.basename()[9:-5]}.pkl"
+    if not gaia_result_path.exists():
+        polygon_points = generate_query_polygon(center, width, height, step_distance)
+        polygon_points = ', '.join(['{0}, {1}'.format(x.ra.to_value(u.degree), x.dec.to_value(u.degree)) for x in polygon_points])
+        query = f"""
+        SELECT source_id, ra, dec, pmra, pmdec, phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag, phot_bp_n_obs, phot_rp_n_obs, phot_variable_flag \
+            FROM gaiadr3.gaia_source \
+            WHERE CONTAINS(
+                POINT('ICRS', gaiadr3.gaia_source.ra, gaiadr3.gaia_source.dec), \
+                POLYGON('ICRS',{polygon_points}))=1 \
+            AND phot_g_mean_mag < {calibration_limits[1] + blend_delta} and phot_g_mean_mag > {calibration_limits[0] - blend_delta};
+        """
+
+        from astroquery.gaia import Gaia
+        job = Gaia.launch_job_async(query)
+        results = job.get_results()
+        Gaia.remove_jobs([job.jobid])
+        pickle.dump(results, open(gaia_result_path, 'wb'))
+    else:
+        results = pickle.load(open(gaia_result_path, 'rb'))
 
     valid_reference = np.logical_and.reduce([
         results['phot_variable_flag'] != 'VARIABLE',
@@ -108,17 +120,26 @@ def query_tap(center, width, height, calibration_limits, blend_delta, step_dista
     return catalog
 
 
-def fetch_gaia(ra_center, dec_center, box_width, box_height, reference_epoch, output_path,
+def fetch_gaia(target, ra_center, dec_center, box_width, box_height, img_path, reference_epoch, output_path,
                calibration_limits, blend_radius, blend_delta):
-    center = SkyCoord(ra_center, dec_center, unit=(u.deg, u.deg))
-
+    if target is not None:
+        center = SkyCoord.from_name(target)
+    else:
+        center = SkyCoord(ra_center, dec_center, unit=(u.deg, u.deg))
+    output_path = Path(output_path)
     start = Time.now()
 
-    catalog = query_tap(center, box_width * u.degree, box_height * u.degree, calibration_limits, blend_delta)
+    catalog = query_tap(output_path, center, box_width * u.degree, box_height * u.degree, calibration_limits, blend_delta)
 
     print(f'fetched in {(Time.now() - start).to(u.second):.3f}')
 
     # Apply proper motion offsets
+    # read header from example image
+    if img_path is not None:
+        img_path = sorted(Path(img_path).files())[0]
+        header = fitsio.read_header(img_path)
+        reference_epoch = header['DATE-OBS']
+
     reference_epoch = Time(reference_epoch, scale='utc')
     delta_years = (reference_epoch - Time('2016-01-01T0:0:0')).to(u.year).value
     catalog['ra_corr'] = catalog['ra_deg'] + catalog['pmra'] * delta_years / (3.6e6 * np.cos(np.radians(catalog['dec_deg'])))
@@ -183,26 +204,26 @@ def fetch_gaia(ra_center, dec_center, box_width, box_height, reference_epoch, ou
 
     output = Table(catalog['source_id', 'ra_corr', 'dec_corr', 'phot_g_mean_mag', 'phot_bp_mean_mag', 'phot_rp_mean_mag',
                            'valid_reference', 'pmra', 'pmdec', 'phot_variable_flag'],
-                   names=['ID', 'RA', 'DEC', 'G_MAG', 'BP_MAG', 'RP_MAG', 'VALID', 'pmRA', 'pmDEC', 'VARIABLE'],
+                   names=['ID', 'RA', 'DEC', 'G_MAG', 'BP_MAG', 'RP_MAG', 'VALID', 'pmRA', 'pmDEC', 'VARIABLE',],
                    meta=meta)
 
     # add Bp-Rp color
     output['BP_RP'] = output['BP_MAG'] - output['RP_MAG']
 
-    # plot the valid reference stars
-    import matplotlib.pyplot as plt
-    plt.rcParams["font.family"] = "Times New Roman"
-    plt.rcParams["font.size"] = 18
-    plt.rcParams["figure.figsize"] = [10, 8]
-    fig, ax = plt.subplots()
-    ax.scatter(output['RA'], output['DEC'], s=np.sqrt(10**(-0.4 * output['G_MAG'])*1e7), c=output['VALID'])
-    ax.set_xlim(center.ra.to_value(u.degree) - box_width / 2, center.ra.to_value(u.degree) + box_width / 2)
-    ax.set_ylim(center.dec.to_value(u.degree) - box_height / 2, center.dec.to_value(u.degree) + box_height / 2)
-    print(f"Max RA {center.ra.to_value(u.degree) + box_width / 2}")
-    plt.show()
+    # # plot the valid reference stars
+    # import matplotlib.pyplot as plt
+    # plt.rcParams["font.family"] = "Times New Roman"
+    # plt.rcParams["font.size"] = 18
+    # plt.rcParams["figure.figsize"] = [10, 8]
+    # fig, ax = plt.subplots()
+    # ax.scatter(output['RA'], output['DEC'], s=np.sqrt(10**(-0.4 * output['G_MAG'])*1e7), c=output['VALID'])
+    # ax.set_xlim(center.ra.to_value(u.degree) - box_width / 2, center.ra.to_value(u.degree) + box_width / 2)
+    # ax.set_ylim(center.dec.to_value(u.degree) - box_height / 2, center.dec.to_value(u.degree) + box_height / 2)
+    # print(f"Max RA {center.ra.to_value(u.degree) + box_width / 2}")
+    # plt.show()
 
     output.sort('G_MAG')
-    output[output['VALID']==True].pprint_all(max_width=-1, max_lines=-1)
+    # output.pprint_all(max_width=-1, max_lines=-1)
 
     output.write(output_path, format='fits', overwrite=True)
     print(f'Wrote {len(output)} reference stars to {output_path}')
@@ -210,12 +231,6 @@ def fetch_gaia(ra_center, dec_center, box_width, box_height, reference_epoch, ou
 
 if __name__ == "__main__":
     parser = ap.ArgumentParser()
-    parser.add_argument('ra',
-                        type=str,
-                        help='Field center RA in degrees.')
-    parser.add_argument('dec',
-                        type=str,
-                        help='Field center Dec in degrees.')
     parser.add_argument('box_width',
                         type=float,
                         metavar='box-width',
@@ -224,19 +239,34 @@ if __name__ == "__main__":
                         type=float,
                         metavar='box-height',
                         help='Box height in degrees.')
+
     parser.add_argument('output',
                         type=str,
                         default='.',
                         help='Path to save the output catalog.')
+    parser.add_argument('--target',
+                        type=str,
+                        default=None,
+                        help='Name of the target object.')
+    parser.add_argument('--ra',
+                        type=str,
+                        help='Field center RA in degrees.')
+    parser.add_argument('--dec',
+                        type=str,
+                        help='Field center Dec in degrees.')
+    parser.add_argument('--img_path',
+                        type=str,
+                        default=None,
+                        help='Example image to extract epoch from.')
     parser.add_argument('--epoch',
                         type=str,
-                        default='2025-01-01T0:0:0',
+                        default='2025-02-01T0:0:0',
                         help='Reference datetime string for proper-motion calculations.')
     parser.add_argument('--calibration-limits',
                         metavar=('MIN', 'MAX'),
                         nargs=2,
                         type=float,
-                        default=(10, 21),
+                        default=(9, 19),
                         help='Minimum and maximum G magnitude to consider for calibration stars')
     parser.add_argument('--blend-radius',
                         type=float,
@@ -248,5 +278,5 @@ if __name__ == "__main__":
                         help='Maximum magnitude delta for a star to be considered as blended')
 
     args = parser.parse_args()
-    fetch_gaia(args.ra, args.dec, args.box_width, args.box_height, args.epoch, args.output,
+    fetch_gaia(args.target, args.ra, args.dec, args.box_width, args.box_height, args.img_path, args.epoch, args.output,
                args.calibration_limits, args.blend_radius, args.blend_delta)

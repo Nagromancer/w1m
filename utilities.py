843c0885442139a2f15600e7313055a798a004c0
Morgan Mitchell <morgan_mitchell@outlook.com>
2025-02-07 10:30:22 +0000

    add 2x2 binning information

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
-plate_scale = 0.394  # arcsec/pixel
+plate_scale = 0.394  # arcsec/pixel  # 0.788 arcsec/pixel for 2x2 binning
 red_gain = 2.011  # e-/ADU
 blue_gain = 1.055  # e-/ADU
-red_read_noise_electrons = 14.306  # e-
-blue_read_noise_electrons = 11.435  # e-
-red_dark_current = 0.9049  # e-/pixel/s
-blue_dark_current = 0.0021  # e-/pixel/s
+red_read_noise_electrons = 14.306  # e-  # 14.28 e- for 2x2 binning
+blue_read_noise_electrons = 11.435  # e-  # 12.96 e- for 2x2 binning
+red_dark_current = 0.9049  # e-/pixel/s   # 3.7867 e-/pixel/s for 2x2 binning
+blue_dark_current = 0.0021  # e-/pixel/s   #  0.0243 e-/pixel/s for 2x2 binning

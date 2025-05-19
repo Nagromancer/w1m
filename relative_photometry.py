115129ebfef55fb250c4deaa193c94f4df4d536a
Morgan Mitchell <morgan_mitchell@outlook.com>
2025-05-19 09:48:58 +0100

    Don't save binned light curve

--- relative_photometry.py
+++ relative_photometry.py
@@ -8,7 +8,7 @@ from astropy.coordinates import SkyCoord
 
 def get_light_curve(phot, target_id, aperture_radius):
     target_phot = phot[phot["ID"] == target_id]
-    time = np.array(target_phot["HJD"])
+    time = np.array(target_phot["BJD"])
     flux = np.array(target_phot[f"FLUX_{aperture_radius}"])
     flux_err = np.array(target_phot[f"FLUX_ERR_{aperture_radius}"])
     return time, flux, flux_err
@@ -23,7 +23,12 @@ plt.rcParams['axes.titlesize'] = 32
 
 camera = "blue"
 bin_size = 5 / 1440  # 10 minutes in days
-dates = ["20250205", "20250206", "20250207", "20250208", "20250213", "20250216", "20250218", "20250219", "20250220", "20250221", "20250222", "20250223", "20250224", "20250225", "20250226", "20250310", "20250317", "20250318", "20250320", "20250321", "20250322", "20250324"]
+dates = ["20250205", "20250206", "20250207", "20250208", "20250213", "20250216", "20250218", "20250219", "20250220",
+         "20250221", "20250222", "20250223", "20250224", "20250225", "20250226", "20250310", "20250317", "20250318",
+         "20250320", "20250321",
+         # "20250322", "20250324", "20250325", "20250326", "20250327", "20250404", "20250406", "20250413", "20250415",
+         "20250417", "20250418", "20250419", "20250420", "20250422", "20250423", "20250424", "20250425", "20250426",
+         "20250427", "20250503", "20250504", "20250514", "20250515"]
 aperture_radius = 5
 targ_id = 1571584539980588544
 
@@ -38,13 +43,15 @@ for date in dates:
     try:
         target = f"Gaia DR3 {targ_id}"
         cat_path = Path(f"/Volumes/SanDisk-2TB-SSD/w1m/reference_catalogues/{target}/{date}-{target}.fits")
-        phot_path = Path(f"/Volumes/SanDisk-2TB-SSD/w1m/dates/{date}/{camera}/{target}/{date}-{target}-{camera}-phot.fits")
+        phot_path = Path(
+            f"/Volumes/SanDisk-2TB-SSD/w1m/dates/{date}/{camera}/{target}/{date}-{target}-{camera}-phot.fits")
         image_path = Path(f"/Volumes/SanDisk-2TB-SSD/w1m/dates/{date}/{camera}/{target}/calibrated")
         phot_table = Table.read(phot_path)
     except FileNotFoundError:
         target = target.replace(" ", "_")
         cat_path = Path(f"/Volumes/SanDisk-2TB-SSD/w1m/reference_catalogues/{target}/{date}-{target}.fits")
-        phot_path = Path(f"/Volumes/SanDisk-2TB-SSD/w1m/dates/{date}/{camera}/{target}/{date}-{target}-{camera}-phot.fits")
+        phot_path = Path(
+            f"/Volumes/SanDisk-2TB-SSD/w1m/dates/{date}/{camera}/{target}/{date}-{target}-{camera}-phot.fits")
         image_path = Path(f"/Volumes/SanDisk-2TB-SSD/w1m/dates/{date}/{camera}/{target}/calibrated")
         phot_table = Table.read(phot_path)
 
@@ -78,9 +85,9 @@ for date in dates:
     for i, star in enumerate(ref_cat):
         _, flux, _ = get_light_curve(phot_table, star["ID"], aperture_radius)
         ref_flux += flux
-    ref_flux  = ref_flux[good_phot_mask]
+    ref_flux = ref_flux[good_phot_mask]
 
-    times -= 2460000
+    # times -= 2460000
     relative_flux = fluxes / ref_flux
     median_rel_flux = np.median(relative_flux)
     relative_flux /= median_rel_flux
@@ -94,7 +101,8 @@ for date in dates:
     # relative_flux = relative_flux[mask]
     # relative_flux_errs = relative_flux_errs[mask]
 
-    flattened, trend = wotan.flatten(time=times, flux=relative_flux, method='biweight', window_length=0.02, return_trend=True)
+    flattened, trend = wotan.flatten(time=times, flux=relative_flux, method='biweight', window_length=0.02,
+                                     return_trend=True)
 
     # bin the light curve
     if bin_size is not None:
@@ -127,7 +135,8 @@ for date in dates:
     binned_std_devs.append(binned_std_dev)
     average_fluxes.append(np.mean(fluxes))
 
-    print(f"{target_mag:.2f} mag: Standard deviation: Trend - {trend_std_dev:.3f} ppt | Relative - {std_dev:.3f} ppt | Binned - {binned_std_dev:.3f} ppt")
+    print(
+        f"{target_mag:.2f} mag: Standard deviation: Trend - {trend_std_dev:.3f} ppt | Relative - {std_dev:.3f} ppt | Binned - {binned_std_dev:.3f} ppt")
     total_binned_times += list(binned_times)
     total_binned_fluxes += list(binned_relative_flux)
     total_binned_flux_errs += list(binned_relative_flux_errs)
@@ -135,19 +144,25 @@ for date in dates:
     total_fluxes += list(relative_flux)
     total_flux_errs += list(relative_flux_errs)
 
+total_times = np.array(total_times)
+total_binned_times = np.array(total_binned_times)
+
 print(f"Total images: {total_images}")
 print(f"Total exposure: {total_images / 120:.2f} hours")
-plt.errorbar(total_times, total_fluxes, yerr=total_flux_errs, fmt='o', color='black', markersize=5, alpha=0.1)
-plt.errorbar(total_binned_times, total_binned_fluxes, yerr=total_binned_flux_errs, fmt='o', color='black', markersize=5)
+# plt.errorbar(total_times - 2460000, total_fluxes, yerr=total_flux_errs, fmt='o', color='black', markersize=5, alpha=0.1)
+plt.errorbar(total_binned_times - 2460000, total_binned_fluxes, yerr=total_binned_flux_errs, fmt='o', color='black',
+             markersize=5)
 # plt.plot(times, trend, color='red', lw=2)
 plt.title(f"Target {target_id} ({target_mag:.2f} mag) - {aperture_radius} px")
-plt.xlabel(f"Time (HJD - 2460000)")
+plt.xlabel(f"Time (BJD - 2460000)")
 plt.ylabel(f"Relative Flux ({'Blue' if camera == 'blue' else 'Red'} Camera)")
+plt.ylim(0.75, 1.15)
 plt.show()
 plt.close()
 
 # perform BLS periodogram
 from astropy.timeseries import BoxLeastSquares
+
 model = BoxLeastSquares(total_times, total_fluxes, total_flux_errs)
 # test for periods between 0.2 and 4 days
 frequencies = np.linspace(0.6, 0.63, 10000)
@@ -166,24 +181,35 @@ print(f"Peak period: {peak_period * 24:.6f} hours")
 total_times = np.array(total_times)
 total_fluxes = np.array(total_fluxes)
 total_flux_errs = np.array(total_flux_errs)
+
+# save the combined light curve as a csv
+combined_lc = Table()
+combined_lc["Time_BJD"] = total_times
+combined_lc["Flux"] = total_fluxes
+combined_lc["Error"] = total_flux_errs
+combined_lc.write(f"/Users/nagro/PycharmProjects/w1m/lcs/combined_lc_{aperture_radius}.csv", overwrite=True)
+print(f"Saved combined light curve to combined_lc_{aperture_radius}.csv")
+
 total_binned_times = np.array(total_binned_times)
 total_binned_fluxes = np.array(total_binned_fluxes)
 total_binned_flux_errs = np.array(total_binned_flux_errs)
+
 total_binned_times -= np.min(total_times)
 total_times -= np.min(total_times)
 
-offset = 0.1
+offset = 0.075
 folded_time = total_times / peak_period
 folded_binned_time = total_binned_times / peak_period
 fig = plt.figure(figsize=(15, 20))
-plt.errorbar((folded_time % 1) * peak_period * 24, total_fluxes + offset * np.floor(folded_time), yerr=total_flux_errs, fmt='o', color='black', markersize=5, alpha=0.1)
-plt.errorbar((folded_binned_time % 1) * peak_period * 24, total_binned_fluxes + offset * np.floor(folded_binned_time), yerr=total_binned_flux_errs, fmt='o', color='black', markersize=5)
+# plt.errorbar((folded_time % 1) * peak_period * 24, total_fluxes + offset * np.floor(folded_time), yerr=total_flux_errs, fmt='o', color='black', markersize=5, alpha=0.1)
+plt.errorbar((folded_binned_time % 1) * peak_period * 24, total_binned_fluxes + offset * np.floor(folded_binned_time),
+             yerr=total_binned_flux_errs, fmt='o', color='black', markersize=5)
 
 plt.title(f"SDSS1234+5606")
 plt.xlabel(f"Phase (hours)")
 plt.ylabel(f"Relative Flux $+$ Offset")
 lower_lim = 0.75
-upper_lim = 9
+upper_lim = 13
 plt.ylim(lower_lim, upper_lim)
 # plt.ylim(lower_lim, 1.2)
 # make a second y axis showing time, because the offset is showing time
@@ -193,16 +219,12 @@ ax2.set_ylabel("$T-T_0$ (days)")
 ax2.set_ylim(lower_lim, upper_lim)
 # ax2.set_ylim(lower_lim, 1.2)
 
-y_ticks_time = np.arange(0, 50, 5)
+y_ticks_time = np.arange(0, 95, 5)
 y_ticks_flux = (y_ticks_time * offset / peak_period) + 1
 ax2.set_yticks(y_ticks_flux)
 ax2.set_yticklabels(y_ticks_time)
 plt.tight_layout()
 plt.show()
-
-
-
-
 
 exit()
 
@@ -221,15 +243,16 @@ plt.grid()
 plt.show()
 
 # plot flux vs magnitude
-plt.scatter(cat[cat["VALID"]]["BP_MAG" if camera == "blue" else "RP_MAG"], average_fluxes, 40, c=cat[cat["VALID"]]["BP_RP"], cmap='coolwarm', clim=(0.0, 2.5))
+plt.scatter(cat[cat["VALID"]]["BP_MAG" if camera == "blue" else "RP_MAG"], average_fluxes, 40,
+            c=cat[cat["VALID"]]["BP_RP"], cmap='coolwarm', clim=(0.0, 2.5))
 # colorbar with label
 # fit a line to log(flux) vs magnitude with np.polyfit
 trend = np.polyfit(cat[cat["VALID"]]["BP_MAG" if camera == "blue" else "RP_MAG"], np.log10(average_fluxes), 1)
-x = np.linspace(np.min(cat[cat["VALID"]]["BP_MAG" if camera == "blue" else "RP_MAG"]), np.max(cat[cat["VALID"]]["BP_MAG" if camera == "blue" else "RP_MAG"]), 100)
+x = np.linspace(np.min(cat[cat["VALID"]]["BP_MAG" if camera == "blue" else "RP_MAG"]),
+                np.max(cat[cat["VALID"]]["BP_MAG" if camera == "blue" else "RP_MAG"]), 100)
 y = 10 ** (trend[0] * x + trend[1])
 plt.plot(x, y, color='black', lw=1)
 print(f"ZP: {-trend[1] / trend[0]:.3f} mag")
-
 
 plt.colorbar(label="${G}_\mathrm{BP} - G_\mathrm{RP}$ (mag)")
 if camera == 'blue':

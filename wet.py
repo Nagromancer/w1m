ea927b0f84311042b5e5806c4e561c95519531a1
Morgan Mitchell <morgan_mitchell@outlook.com>
2025-10-24 14:13:46 +0100

    initial commit

--- wet.py
+++ wet.py
@@ -1,10 +1,10 @@
 from path import Path
 import fitsio
 
-date = 20251015
+date = 20251022
 
 # set up paths
-base_dir = Path(f"/Volumes/SanDisk-2TB-SSD/w1m/dates/{date}")
+base_dir = Path(f"/home/ops/{date}")
 dark_dir = base_dir / "dark"
 bias_dir = base_dir / "bias"
 flat_dir = base_dir / "flat"
@@ -16,10 +16,9 @@ for directory in [dark_dir, bias_dir, flat_dir, science_dir]:
         directory.mkdir()
 
 # common metadata
-phot_filter = "bg40"
-observer = "Morgan Mitchell"
-telcode = "warw"
-object_name = "G2938"
+phot_filter = "V"
+observer = "Ioannis Apergis"
+telcode = "twist"
 
 # move files to appropriate directories
 for file in base_dir.files("*.fits"):
@@ -29,32 +28,38 @@ for file in base_dir.files("*.fits"):
         file.move(bias_dir / file.name)
     elif "flat" in file.name.lower():
         file.move(flat_dir / file.name)
-    elif "Gaia" in file.name:
+    else:
         file.move(science_dir / file.name)
 
+# get all unique target names from science files
+# example filename: PB_7225-20251022211944112.fits
+# object name is PB_7225
+object_names = set()
+for file in science_dir.files("*.fits"):
+    target = file.basename()[:-23]
+    object_names.add(target)
+object_names = list(object_names)
+
 # rename files in science directory
-for i, file in enumerate(sorted(science_dir.files("*.fits"))):
-    if "BLUE" in file.name:
+for object_name in object_names:
+    for i, file in enumerate(sorted(science_dir.files(f"*{object_name}*.fits"))):
         new_name = f"{telcode}{date}_{object_name}_{phot_filter}_{i+1:04d}.fits"
         file.rename(science_dir / new_name)
 
 # rename files in flat directory
 for i, file in enumerate(sorted(flat_dir.files("*.fits"))):
-    if "BLUE" in file.name:
-        new_name = f"{telcode}{date}_flat_{phot_filter}_{i+1:04d}.fits"
-        file.rename(flat_dir / new_name)
+    new_name = f"{telcode}{date}_flat_{phot_filter}_{i+1:04d}.fits"
+    file.rename(flat_dir / new_name)
 
 # rename files in dark directory
 for i, file in enumerate(sorted(dark_dir.files("*.fits"))):
-    if "BLUE" in file.name:
-        new_name = f"{telcode}{date}_dark_{i+1:04d}.fits"
-        file.rename(dark_dir / new_name)
+    new_name = f"{telcode}{date}_dark_{i+1:04d}.fits"
+    file.rename(dark_dir / new_name)
 
 # rename files in bias directory
 for i, file in enumerate(sorted(bias_dir.files("*.fits"))):
-    if "BLUE" in file.name:
-        new_name = f"{telcode}{date}_bias_{i+1:04d}.fits"
-        file.rename(bias_dir / new_name)
+    new_name = f"{telcode}{date}_bias_{i+1:04d}.fits"
+    file.rename(bias_dir / new_name)
 
 # add OBSERVER to headers
 for directory in [dark_dir, bias_dir, flat_dir, science_dir]:
